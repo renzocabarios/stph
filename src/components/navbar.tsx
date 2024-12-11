4475640c5cbebd6da026553bb8f2db2dda2d43b7
renzocabarios <renzo.cabarios@gmail.com>
2024-12-11 18:11:11 +0800

    Update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { Menu } from "lucide-react";
-
+import Image from "next/image";
 export default function Navbar() {
   return (
     <div className="w-full flex justify-center sticky top-0 bg-transparent p-8 border-b border-gray-700 bg-red-600">
-      <div className="flex items-center justify-between max-w-[1440px] w-full">
+      <div className="flex flex-col items-center gap-4  max-w-[1440px] w-full">
         <div className="logo">
-          <img src="/stph.png" alt="Logo" className="h-11" />
+          <Image src="/stph.png" alt="Logo" className="h-11"></Image>
         </div>
-        <Menu size={30} className="text-white md:hidden" />
-        <div className="md:flex items-center gap-4 text-white text-2xl hidden">
+        <div className="flex justify-evenly gap-4 text-white text-md md:text-2xl">
           <p className="hover:text-yellow-300 cursor-pointer">Blog</p>
           <p className="hover:text-yellow-300 cursor-pointer">Events</p>
           <p className="hover:text-yellow-300 cursor-pointer">Hire</p>
@@ -18,4 +16,17 @@ export default function Navbar() {
       </div>
     </div>
   );
+}
+{
+  /* <div className="flex items-center justify-between max-w-[1440px] w-full">
+<div className="logo">
+  <img src="/stph.png" alt="Logo" className="h-11" />
+</div>
+<div className="md:flex items-center gap-4 text-white text-2xl hidden">
+  <p className="hover:text-yellow-300 cursor-pointer">Blog</p>
+  <p className="hover:text-yellow-300 cursor-pointer">Events</p>
+  <p className="hover:text-yellow-300 cursor-pointer">Hire</p>
+  <p className="hover:text-yellow-300 cursor-pointer">About</p>
+</div>
+</div> */
 }
