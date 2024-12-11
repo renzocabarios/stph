3fe9aa0860587fb5a8f2234e06c57c611d782045
renzocabarios <renzo.cabarios@gmail.com>
2024-12-11 18:12:04 +0800

    Update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Navbar from "@/components/navbar";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
+import Mission from "@/views/mission";
+import Hero from "@/views/hero";
+import Partners from "@/views/partners";
+import JoinUs from "@/views/join-us";
+import Faq from "@/views/faq";
+import BottomNav from "@/views/bottom-nav";
 export default function Home() {
   return (
     <>
@@ -10,62 +14,14 @@ export default function Home() {
 
       {/* Main Content - 1st Page*/}
 
-      <div className="w-full flex justify-end py-40 px-[7vw]">
-        {/* <div className="flex flex-col space-y-8 mr-12 mt-[100px] py-[35px]">
-          <div className="flex justify-center items-center border-[2px] border-[#2D2E48] rounded-[20px] w-[60px] h-[60px] bg-[rgba(109,103,103,0.23)]">
-            <Button
-              variant="ghost"
-              className="bg-transparent hover:bg-transparent p-0"
-            >
-              <Image src="/learn.svg" alt="Icon 1" width={100} height={100} />
-            </Button>
-          </div>
+      <Hero />
 
-          <div className="flex justify-center items-center border-[2px] border-[#2D2E48] rounded-[20px] w-[60px] h-[60px] bg-[rgba(109,103,103,0.23)]">
-            <Button
-              variant="ghost"
-              className="bg-transparent hover:bg-transparent p-0"
-            >
-              <Image src="/earn.svg" alt="Icon 2" width={100} height={100} />
-            </Button>
-          </div>
+      <Mission />
+      <Partners />
 
-          <div className="flex justify-center items-center border-[2px] border-[#2D2E48] rounded-[20px] w-[60px] h-[60px] bg-[rgba(109,103,103,0.23)]">
-            <Button
-              variant="ghost"
-              className="bg-transparent hover:bg-transparent p-0"
-            >
-              <Image src="/build.svg" alt="Icon 3" width={30} height={30} />
-            </Button>
-          </div>
-
-          <div className="flex justify-center items-center border-[2px] border-[#2D2E48] rounded-[20px] w-[60px] h-[60px] bg-[rgba(109,103,103,0.23)]">
-            <Button
-              variant="ghost"
-              className="bg-transparent hover:bg-transparent p-0"
-            >
-              <Image src="/chill.svg" alt="Icon 4" width={100} height={100} />
-            </Button>
-          </div>
-        </div> */}
-
-        <div className="flex flex-col gap-4 text-white">
-          <p className="text-3xl md:text-6xl font-bold">
-            Join the
-            <br />
-            Web3 Revolution
-          </p>
-          <p className=" text-md font-medium">
-            At Superteam Philippines, we are dedicated to propelling the most
-            promising projects within the Solana ecosystem to new heights. As
-            the official Solana Superteam in the Philippines, we are uniquely
-            positioned to support and foster innovation in this rapidly evolving
-            space.
-          </p>
-        </div>
-      </div>
-
-      {/* Main Content - 2nd Page*/}
+      <JoinUs />
+      <Faq />
+      <BottomNav />
 
       {/* <div className="w-full flex justify-center py-40">
         <div
