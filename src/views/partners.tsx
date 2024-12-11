259b8ae8b99a11e19f8bfe7c268580defb008f31
renzocabarios <renzo.cabarios@gmail.com>
2024-12-11 18:12:08 +0800

    Add partners

---
+++ src/views/partners.tsx
@@ -0,0 +1,14 @@
+export default function Partners() {
+  return (
+    <div className="w-full flex flex-col gap-4  p-8">
+      <p className="text-sm font-medium text-center">OUR COMMUNITY PARTNERS</p>
+
+      <div className="w-full flex items-center justify-between">
+        <p>Sponsor</p>
+        <p>Sponsor</p>
+        <p>Sponsor</p>
+        <p>Sponsor</p>
+      </div>
+    </div>
+  );
+}
